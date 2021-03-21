56eaf88b4a8fce18db97694102e658f855c5376a
Fionna <fionnacst@gmail.com>
2021-03-21 14:31:09 +0800

    Remove commented code

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -22,51 +22,6 @@ export default function() {
   )
 }
 
-// function BlockNumber() {
-//   const { chainId, library } = useWeb3React()
-
-//   const [blockNumber, setBlockNumber] = React.useState<number>()
-//   React.useEffect((): any => {
-//     if (!!library) {
-//       let stale = false
-
-//       library
-//         .getBlockNumber()
-//         .then((blockNumber: number) => {
-//           if (!stale) {
-//             setBlockNumber(blockNumber)
-//           }
-//         })
-//         .catch(() => {
-//           if (!stale) {
-//             setBlockNumber(null)
-//           }
-//         })
-
-//       const updateBlockNumber = (blockNumber: number) => {
-//         setBlockNumber(blockNumber)
-//       }
-//       library.on('block', updateBlockNumber)
-
-//       return () => {
-//         stale = true
-//         library.removeListener('block', updateBlockNumber)
-//         setBlockNumber(undefined)
-//       }
-//     }
-//   }, [library, chainId]) // ensures refresh if referential identity of library doesn't change across chainIds
-
-//   return (
-//     <>
-//       <span>Block Number</span>
-//       <span role="img" aria-label="numbers">
-//         🔢
-//       </span>
-//       <span>{blockNumber === null ? 'Error' : blockNumber ?? ''}</span>
-//     </>
-//   )
-// }
-
 function App() {
   return (
     <>
